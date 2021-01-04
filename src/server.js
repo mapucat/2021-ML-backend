f0320f6c722d39f665834ae1613774aa79b69df3
maprodri <maprodri@bancolombia.com.co>
2021-01-03 23:35:29 -0500

    increment: Agrega el archivo server.js  - Agrega el archivo principal server.js, el cuál será el que guardará la configuración del servidor.

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { ErrorResponse } = require('./models/error-response.model');
+const app = express();
+const routes = require('./routes/routes');
+
+app.use('/api', routes);
+
+app.all('**', (req, res) => {
+  'use strict';
+  res.status(404).send(new ErrorResponse(404, req.url));
+});
+
+app.listen(3000);
+
+
+module.exports = {
+  app
+};
