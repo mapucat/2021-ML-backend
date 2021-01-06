52970a793d1c6dbff98a8ce07726752aa2856465
maprodri <maprodri@bancolombia.com.co>
2021-01-05 23:21:30 -0500

    increment: Configura el control de acceso (CORS)

--- src/server.js
+++ src/server.js
@@ -3,6 +3,21 @@ const { ErrorResponse } = require('./models/error-response.model');
 const app = express();
 const routes = require('./routes/routes');
 
+// Agrega los headers
+app.use((req, res, next) => {
+
+  // Adiciona la url del frontend
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Específica los métodos que se van a permitir
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // Especifica los headers que se van apermitir
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  next();
+});
+
 app.use('/api', routes);
 
 app.all('**', (req, res) => {
